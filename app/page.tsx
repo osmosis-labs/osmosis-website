aff22fd18e7b50ccad225717d6e1716a5d3d3e2b
fabry-js <vivaldifabrizio10@gmail.com>
2024-03-06 12:33:26 +0100

    feat: add layout with placeholders

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,39 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="mt-10.5 mb-45 flex flex-col items-center">
+      <section className="max-w-content flex h-[600px] w-[1440px] items-center justify-center bg-osmoverse-800">
+        hero-section.tsx
+      </section>
+      <section className="max-w-content mt-20 flex h-[526px] w-[1440px] items-center justify-center bg-osmoverse-800">
+        token-stats.tsx
+      </section>
+      <section className="max-w-content mt-47 flex h-[875px] w-[1440px] items-center justify-center bg-osmoverse-800">
+        explore-assets.tsx
+      </section>
+      <section className="mt-14.5 flex h-[1141px] w-full items-center justify-center bg-osmoverse-800">
+        stats-with-tweets.tsx
+      </section>
+      <section className="max-w-content mt-19.5 flex h-[561px] w-[1440px] items-center justify-center bg-osmoverse-800">
+        osmo-cta.tsx
+      </section>
+      <section className="mt-58.25 flex h-[908px] w-[1440px] items-center justify-center bg-osmoverse-800">
+        stake-section.tsx
+      </section>
+      <section className="mt-42 flex h-[1018px] w-full items-center justify-center bg-osmoverse-800">
+        earn-section.tsx
+      </section>
+      <section className="mt-35 flex h-[559px] w-[1440px] items-center justify-center bg-osmoverse-800">
+        vote-section.tsx
+      </section>
+      <section className="mt-65 flex h-[500px] w-[1440px] items-center justify-center bg-osmoverse-800">
+        contacts-section.tsx
+      </section>
+      <section className="mt-30 flex h-[890px] w-[1440px] items-center justify-center bg-osmoverse-800">
+        help-section.tsx
+      </section>
+      <section className="mt-28 flex h-[466px] w-[1440px] items-center justify-center bg-osmoverse-800">
+        trade-section.tsx
+      </section>
+    </main>
+  );
 }
