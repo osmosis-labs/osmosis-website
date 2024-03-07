22f73f271c8c2a2a648517208f7b7e439d8c6eb2
fabry-js <vivaldifabrizio10@gmail.com>
2024-03-07 22:55:05 +0100

    feat: update logo link and cta

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -103,7 +103,9 @@ export default function Navbar() {
   const [_, setIsOpen] = useState(false);
   return (
     <header className="after:bg-gradient-navbar-border relative flex items-center justify-between px-10 py-3 after:absolute after:bottom-0 after:h-[1px] after:w-full after:content-['']">
-      <Image src={osmosisLogo} alt="Osmosis Logo" width={130} height={32} />
+      <Link href={"/"}>
+        <Image src={osmosisLogo} alt="Osmosis Logo" width={130} height={32} />
+      </Link>
       <nav
         className={cn(
           "relative flex items-center gap-2 rounded-full border border-[#9084CF40] px-4 py-2",
@@ -199,12 +201,14 @@ export default function Navbar() {
           </Transition>
         </Menu>
       </nav>
-      <button
+      <Link
+        href={"https://app.osmosis.zone"}
         type="button"
+        target="_blank"
         className="inline-flex items-center justify-center rounded-xl bg-wosmongton-50 px-4 py-2.5 transition-colors hover:bg-wosmongton-800"
       >
         <span className="font-body1 text-body1">Open the App</span>
-      </button>
+      </Link>
     </header>
   );
 }
