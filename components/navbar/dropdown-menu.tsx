3b384e7d3103452821af8a13b2dde6e4e4843cea
fabry-js <vivaldifabrizio10@gmail.com>
2024-03-14 00:13:05 +0100

    fix: fix dropdown menu positioning

--- components/navbar/dropdown-menu.tsx
+++ components/navbar/dropdown-menu.tsx
@@ -97,58 +97,60 @@ export function DropwdownMenu() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute -right-4 flex w-[342px] origin-top flex-col gap-6 overflow-hidden rounded-2xl border border-osmoverse-750 bg-[#160F38] p-8 max-lg:bottom-16 lg:-right-7.5 lg:top-12">
-          <DiamondGradient
-            colors={["#FF8642", "#EB26EB", "#492CE1"]}
-            height={450}
-            width={350}
-            paddingLeft={50}
-            paddingTop={50}
-            className="absolute bottom-56 right-36 rotate-[25deg]"
-          />
-          <div className="grid grid-cols-2 gap-x-4 gap-y-6">
-            {menuLinks.map(({ name, links }) => (
-              <div key={name} className="col-span-1 flex flex-col gap-2.5">
-                <span className="body2 opacity-60">{name}</span>
-                <div className="flex flex-col gap-4">
-                  {links.map(({ label, href }) => (
-                    <DropdownMenuLink key={label} href={href} label={label} />
-                  ))}
-                </div>
-              </div>
-            ))}
-          </div>
-          <div className="flex items-center justify-between gap-2.5">
-            <div className="h-[1px] flex-1 bg-osmoverse-750" />
-            <Image
-              src={"/assets/icons/star-divider.svg"}
-              alt="Divider"
-              width={14}
-              height={14}
+        <div className="absolute inset-x-0 flex w-full justify-center max-lg:bottom-16 lg:-right-7.5 lg:top-16">
+          <Menu.Items className="relative flex w-[342px] origin-top flex-col gap-6 overflow-hidden rounded-2xl border border-osmoverse-750 bg-[#160F38] p-8">
+            <DiamondGradient
+              colors={["#FF8642", "#EB26EB", "#492CE1"]}
+              height={450}
+              width={350}
+              paddingLeft={50}
+              paddingTop={50}
+              className="absolute bottom-56 right-36 rotate-[25deg]"
             />
-            <div className="h-[1px] flex-1 bg-osmoverse-750" />
-          </div>
-          <div className="flex flex-col gap-2.5">
-            <span className="body2 opacity-60">Connect with us</span>
-            <div className="flex items-center gap-1">
-              {socials.map(({ icon, name, href }) => (
-                <Link
-                  href={href}
-                  key={name}
-                  className="rounded-full p-1.5 transition-colors hover:bg-wosmongton-50"
-                >
-                  <Image
-                    src={icon}
-                    alt={`${name} social link`}
-                    width={20}
-                    height={20}
-                    className="max-h-5"
-                  />
-                </Link>
+            <div className="grid grid-cols-2 gap-x-4 gap-y-6">
+              {menuLinks.map(({ name, links }) => (
+                <div key={name} className="col-span-1 flex flex-col gap-2.5">
+                  <span className="body2 opacity-60">{name}</span>
+                  <div className="flex flex-col gap-4">
+                    {links.map(({ label, href }) => (
+                      <DropdownMenuLink key={label} href={href} label={label} />
+                    ))}
+                  </div>
+                </div>
               ))}
             </div>
-          </div>
-        </Menu.Items>
+            <div className="flex items-center justify-between gap-2.5">
+              <div className="h-[1px] flex-1 bg-osmoverse-750" />
+              <Image
+                src={"/assets/icons/star-divider.svg"}
+                alt="Divider"
+                width={14}
+                height={14}
+              />
+              <div className="h-[1px] flex-1 bg-osmoverse-750" />
+            </div>
+            <div className="flex flex-col gap-2.5">
+              <span className="body2 opacity-60">Connect with us</span>
+              <div className="flex items-center gap-1">
+                {socials.map(({ icon, name, href }) => (
+                  <Link
+                    href={href}
+                    key={name}
+                    className="rounded-full p-1.5 transition-colors hover:bg-wosmongton-50"
+                  >
+                    <Image
+                      src={icon}
+                      alt={`${name} social link`}
+                      width={20}
+                      height={20}
+                      className="max-h-5"
+                    />
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </Menu.Items>
+        </div>
       </Transition>
     </Menu>
   );
