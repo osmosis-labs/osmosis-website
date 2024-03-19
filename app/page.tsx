c0c849cbf26aac81ed840f9ae63deda5c42bcfeb
fabry-js <vivaldifabrizio10@gmail.com>
2024-03-19 14:17:57 +0100

    fix: fix mt & mb of main

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import TokenStatsSection from "@/components/sections/token-stats";
 
 export default function Home() {
   return (
-    <main className="mb-21.5 mt-4 flex w-full flex-col lg:mb-13 lg:mt-8.5 xl:mb-16.5 xl:mt-11 2xl:mt-6.5 3xl:mb-20">
+    <main className="mb-8 mt-6 flex w-full flex-col sm:mb-5.5 sm:mt-4 md:mb-14 md:mt-5.5 lg:mt-4.5 xl:mb-18 xl:mt-11 2xl:mb-20 2xl:mt-6.5">
       <HeroSection />
       <TokenStatsSection />
       {/* <section className="mt-47 flex h-[875px] w-[1440px] max-w-content items-center justify-center bg-osmoverse-800">
