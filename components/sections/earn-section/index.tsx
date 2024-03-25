15370bf243430b1a611f8325fc30a7095c809f0b
fabry-js <vivaldifabrizio10@gmail.com>
2024-03-25 11:30:37 +0100

    fix(earn): update headline font weight

--- components/sections/earn-section/index.tsx
+++ components/sections/earn-section/index.tsx
@@ -136,7 +136,7 @@ export default function EarnSection() {
               Put your assets to work
             </span>
           </div>
-          <h2 className="bg-earn-headline font-h1 text-3.5xl leading-[38px] -tracking-[0.64px] md:text-4xl md:leading-[43px] md:-tracking-[0.72px] lg:text-5xl lg:leading-[53px] lg:-tracking-[0.96px] xl:text-[56px] xl:leading-[61px] xl:-tracking-[1.12px] 2xl:text-[64px] 2xl:leading-[70px] 2xl:-tracking-[1.28px]">
+          <h2 className="bg-earn-headline font-h1 text-3.5xl font-medium leading-[38px] -tracking-[0.64px] md:text-4xl md:leading-[43px] md:-tracking-[0.72px] lg:text-5xl lg:leading-[53px] lg:-tracking-[0.96px] xl:text-[56px] xl:leading-[61px] xl:-tracking-[1.12px] 2xl:text-[64px] 2xl:leading-[70px] 2xl:-tracking-[1.28px]">
             Unlock Your <br /> Earning Potential.
           </h2>
         </div>
