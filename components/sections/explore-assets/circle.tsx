6bbf0452e830a31981624cd15e503a66f5e03df6
fabry-js <vivaldifabrizio10@gmail.com>
2024-03-28 16:58:47 +0100

    feat: WIP use css for circle gen and fix section placement

---
+++ components/sections/explore-assets/circle.tsx
@@ -0,0 +1,120 @@
+import { formatPretty } from "@/lib/formatting";
+import { cn } from "@/lib/utils";
+import { RatePretty } from "@keplr-wallet/unit";
+import Image from "next/image";
+import { CSSProperties } from "react";
+
+interface IAsset {
+  iconUri: string;
+  name: string;
+  variation: RatePretty;
+  important: boolean;
+}
+
+export type TAsset = Partial<IAsset>;
+
+interface CircleProps {
+  list: TAsset[];
+  className?: string;
+}
+export default function Circle({ list, className }: CircleProps) {
+  return (
+    <div className={cn("rounded-full", className)}>
+      {list.map((asset, i) => (
+        <Element
+          key={`list ${i}`}
+          {...asset}
+          style={{
+            top: `calc(
+              50% - (
+                50% * sin(
+                  var(--angle) * var(--index) + var(--offset) * -1
+                )
+              )
+            )`,
+            right: `calc(
+              50% + (
+                50% * cos(
+                  var(--angle) * var(--index) + var(--offset) * -1
+                )
+              )
+            )`,
+            transform: "translate(50%, -50%)",
+            //@ts-ignore
+            "--index": i,
+          }}
+        />
+      ))}
+    </div>
+  );
+}
+
+function Element({
+  className,
+  style,
+  iconUri,
+  name,
+  variation,
+}: {
+  className?: string;
+  style?: CSSProperties;
+} & TAsset) {
+  const isPositive = (variation ?? new RatePretty(0)).toDec().isPositive();
+  return (
+    <div
+      style={style}
+      className={cn(
+        "absolute z-10 flex h-10 w-10 -translate-y-1/2 translate-x-1/2 flex-col items-center justify-center md:h-20 md:w-20 lg:h-[110px] lg:w-24",
+        className,
+      )}
+    >
+      {iconUri && (
+        <Image
+          src={iconUri}
+          alt={`${name} logo`}
+          width={40}
+          height={40}
+          className="md:h-12 md:w-12 lg:h-18 lg:w-18"
+        />
+      )}
+      {variation && (
+        <div
+          className={cn(
+            "-mt-1.5 flex h-4 w-max items-center justify-center rounded-full p-1 md:h-5",
+            {
+              "bg-rust-500": !isPositive,
+              "bg-malachite-200": isPositive,
+            },
+          )}
+        >
+          {isPositive ? (
+            <Image
+              src={"/assets/explore-assets/indicator-up.svg"}
+              alt="Indicator Up"
+              width={12}
+              height={12}
+            />
+          ) : (
+            <Image
+              src={"/assets/explore-assets/indicator-down.svg"}
+              alt="Indicator Down"
+              width={12}
+              height={12}
+            />
+          )}
+          <span
+            className={cn(
+              "text-[10px] font-medium leading-[4.6px] -tracking-[0.132px] sm:text-xs",
+              {
+                "text-[#003F47]": isPositive,
+                "text-[#4A2706]": !isPositive,
+              },
+            )}
+          >
+            {formatPretty(variation)}
+          </span>
+        </div>
+      )}
+    </div>
+  );
+}
--- components/sections/explore-assets/index.tsx
+++ components/sections/explore-assets/index.tsx
@@ -1,17 +1,6 @@
-import { formatPretty } from "@/lib/formatting";
-import { cn } from "@/lib/utils";
-import { Dec, RatePretty } from "@keplr-wallet/unit";
+import Circle, { TAsset } from "@/components/sections/explore-assets/circle";
+import { RatePretty } from "@keplr-wallet/unit";
 import Image from "next/image";
-import { CSSProperties } from "react";
-
-interface IAsset {
-  iconUri: string;
-  name: string;
-  variation: RatePretty;
-  important: boolean;
-}
-
-type TAsset = Partial<IAsset>;
 
 const assets: TAsset[] = [
   {
@@ -27,32 +16,32 @@ const assets: TAsset[] = [
     important: true,
   },
   {
+    iconUri: "/assets/icons/dym.svg",
+    name: "Dymension",
+    variation: new RatePretty(3),
+    important: true,
+  },
+  {
+    iconUri: "/assets/icons/tia.svg",
+    name: "Celestia",
+    variation: new RatePretty(1.28),
+    important: true,
+  },
+  {
+    iconUri: "/assets/icons/tia.svg",
+    name: "Celestia",
+    variation: new RatePretty(1.28),
+    important: true,
+  },
+  {
+    iconUri: "/assets/icons/tia.svg",
+    name: "Celestia",
+    variation: new RatePretty(1.28),
+    important: true,
+  },
+  {
     iconUri: "/assets/icons/pepe.svg",
     name: "PEPE",
-    variation: new RatePretty(-9),
-    important: true,
-  },
-  {
-    iconUri: "/assets/icons/tia.svg",
-    name: "Celestia",
-    variation: new RatePretty(1.28),
-    important: true,
-  },
-  {
-    iconUri: "/assets/icons/tia.svg",
-    name: "Celestia",
-    variation: new RatePretty(1.28),
-    important: true,
-  },
-  {
-    iconUri: "/assets/icons/tia.svg",
-    name: "Celestia",
-    variation: new RatePretty(1.28),
-    important: true,
-  },
-  {
-    iconUri: "/assets/icons/tia.svg",
-    name: "Celestia",
     variation: new RatePretty(1.28),
     important: true,
   },
@@ -124,41 +113,19 @@ const assets: TAsset[] = [
   },
 ];
 
-function roundAssets(assets: TAsset[]) {
-  const copy = [...assets];
-  if (copy.length % 2 === 0) return copy;
-  copy.splice(-(copy.length / 4), 0, { name: "void" });
-  return copy;
-}
-
 export default function ExploreAssets() {
-  const otherAssets = assets.filter((asset) => asset.important === false);
   const importantAssets = assets.filter((asset) => asset.important === true);
+  const otherAssets = assets.filter((asset) => asset.important === false);
 
   return (
-    <section className="relative mt-16 flex flex-col items-center justify-center gap-3.5 sm:mt-20 sm:gap-8 md:mt-28 md:h-[590px] md:w-[640px] md:justify-end md:self-stretch lg:mt-8 lg:h-[878px] lg:w-[960px] xl:mt-28 xl:h-[986px] xl:w-[1152px] xl:overflow-hidden 2xl:mt-42 2xl:h-[1044px] 2xl:w-[1440px] 2xl:overflow-visible 2xl:px-6">
-      <Heart
-        assetsLists={[
-          {
-            list: otherAssets,
-            radius: [150, 188, 280, 395, 380, 380],
-          },
-          {
-            list: importantAssets,
-            radius: [85, 102, 191, 260, 260, 260],
-          },
-        ]}
-      />
-      {/* <div className="relative flex h-[360px] w-[360px] items-center justify-center">
-        <Circle className="absolute" elements={otherAssets} radius={150} />
-        <Circle className="absolute" elements={importantAssets} radius={85} />
-      </div> */}
+    <section className="relative mt-16 flex flex-col items-center justify-center gap-3.5 sm:mt-20 sm:gap-8 md:mt-28 md:justify-end md:self-stretch lg:mt-8 lg:w-[960px] xl:mt-28 xl:h-[986px] xl:w-[1152px] xl:overflow-hidden 2xl:mt-42 2xl:h-[1044px] 2xl:w-[1440px] 2xl:overflow-visible 2xl:px-6">
+      <Heart assetsLists={[otherAssets, importantAssets]} />
       <div className="relative z-10 flex flex-col items-center justify-center gap-6 md:gap-8 2xl:px-6">
         <div className="flex flex-col items-center justify-center gap-4 text-center">
-          <h3 className="bg-explore-assets-headline font-poppins w-[290px] text-3.5xl font-medium leading-[38px] -tracking-[0.64px] sm:w-[390px] md:w-[420px] md:text-4xl md:leading-[41px] md:-tracking-[0.72px] lg:w-[594px] lg:text-5xl lg:leading-[55px] lg:-tracking-[0.96px] xl:w-[660px] xl:text-[56px] xl:leading-[64px] xl:-tracking-[1.12px] 2xl:w-[768px] 2xl:text-[64px] 2xl:leading-[70px] 2xl:-tracking-[1.28px]">
+          <h3 className="bg-explore-assets-headline w-[290px] font-poppins text-3.5xl font-medium leading-[38px] -tracking-[0.64px] sm:w-[390px] md:w-[420px] md:text-4xl md:leading-[41px] md:-tracking-[0.72px] lg:w-[594px] lg:text-5xl lg:leading-[55px] lg:-tracking-[0.96px] xl:w-[660px] xl:text-[56px] xl:leading-[64px] xl:-tracking-[1.12px] 2xl:w-[768px] 2xl:text-[64px] 2xl:leading-[70px] 2xl:-tracking-[1.28px]">
             The best opportunities are on Osmosis.
           </h3>
-          <span className="text-alpha-60 max-w-[223px] font-light leading-[25px] md:max-w-none md:leading-[22px] lg:text-xl">
+          <span className="max-w-[223px] font-light leading-[25px] text-alpha-60 md:max-w-none md:leading-[22px] lg:text-xl">
             Discover and trade 190+ assets and counting.
           </span>
         </div>
@@ -182,229 +149,37 @@ export default function ExploreAssets() {
   );
 }
 
-function Heart({
-  assetsLists,
-}: {
-  assetsLists: { list: TAsset[]; radius: number[] }[];
-}) {
+function Heart({ assetsLists }: { assetsLists: TAsset[][] }) {
   const voidAssets = Array(28).fill({
     iconUri: "/assets/explore-assets/void-asset.svg",
   }) as TAsset[];
 
   return (
-    <>
-      {/* <div className="relative flex h-[360px] w-[360px] items-center justify-center sm:h-[440px] sm:w-[440px] md:h-[368px] md:w-[640px] lg:h-[610px] lg:w-[956px] xl:h-[700px] xl:w-[1054px] 2xl:h-[662px]">
-        {assetsLists.map(({ list, radius }, i) => (
-          <Circle
-            key={`heart list ${i}`}
-            className="absolute"
-            elements={list}
-            radius={radius[0]}
-          />
-        ))}
-      </div> */}
-      <div className="relative flex h-[360px] w-[360px] items-center justify-center sm:hidden">
-        {assetsLists.map(({ list, radius }, i) => (
-          <Circle
-            key={`heart list ${i}`}
-            className="absolute"
-            elements={roundAssets(list)}
-            radius={radius[0]}
-          />
-        ))}
-      </div>
-      {/**sm */}
-      <div className="relative hidden h-[440px] w-[440px] items-center justify-center sm:flex md:hidden">
-        {assetsLists.map(({ list, radius }, i) => (
-          <Circle
-            key={`heart list ${i}`}
-            className="absolute"
-            elements={roundAssets(list)}
-            radius={radius[1]}
-          />
-        ))}
-      </div>
-      {/**md */}
-      <div className="absolute top-[93px] hidden h-[388px] w-[640px] items-center justify-center md:flex lg:hidden">
-        {assetsLists.map(({ list, radius }, i) => (
-          <Circle
-            key={`heart list ${i}`}
-            className="absolute"
-            elements={list}
-            radius={radius[2]}
-            isSemi
-          />
-        ))}
-      </div>
-      {/**lg */}
-      <div className="absolute top-[203px] hidden h-[610px] w-[956px] items-center justify-center lg:flex xl:hidden">
-        {assetsLists.map(({ list, radius }, i) => (
-          <Circle
-            key={`heart list ${i}`}
-            className="absolute"
-            elements={list}
-            radius={radius[3]}
-            isSemi
-          />
-        ))}
-      </div>
-      {/**xl */}
-      <div className="absolute top-[233px] hidden h-[700px] w-[1054px] items-center justify-center xl:flex 2xl:hidden">
-        {assetsLists.map(({ list, radius }, i) => (
-          <Circle
-            key={`heart list ${i}`}
-            className="absolute z-20"
-            elements={list}
-            radius={radius[4]}
-            isSemi
-          />
-        ))}
-        <Circle className="absolute" elements={voidAssets} radius={500} />
-        <div className="void-assets-mask absolute z-10 h-[calc(100%+200px)] w-screen" />
-      </div>
-      {/**2xl */}
-      <div className="absolute hidden h-[960px] w-[1100px] items-center justify-center 2xl:flex">
-        {assetsLists.map(({ list, radius }, i) => (
-          <Circle
-            key={`heart list ${i}`}
-            className="absolute"
-            elements={list}
-            radius={radius[5]}
-            isSemi
-          />
-        ))}
-        <Circle className="absolute" elements={voidAssets} radius={496} />
-        <div className="void-assets-mask absolute z-10 h-[calc(100%+200px)] w-screen" />
-      </div>
-      <div className="absolute left-1/2 top-1/2 h-[353px] w-[362px] -translate-x-1/2 -translate-y-1/2 sm:h-[440px] sm:w-[440px] md:h-[620px] md:w-[620px] lg:h-[800px] lg:w-[800px] xl:z-10 2xl:h-[950px] 2xl:w-[1000px]">
-        <Image
-          src={"/assets/explore-assets/heart-bg-gradient.svg"}
-          alt="Heart bg"
-          width={1000}
-          height={950}
-          className="-translate-y-[calc(50%-40px)] sm:-translate-y-[calc(50%-72px)] md:-translate-y-16 lg:-translate-y-0"
+    <div className="relative">
+      <div className="relative z-10 flex h-[380px] w-[360px] items-center justify-center sm:h-[442px] sm:w-[440px] md:h-[388px] md:w-[628px] lg:h-[610px] lg:w-[956px] xl:flex xl:h-[700px] xl:w-[1054px] 2xl:h-[662px]">
+        <Circle
+          className="external-circle absolute flex h-[300px] w-[300px] items-center justify-center sm:top-10 sm:h-[376px] sm:w-[376px] md:h-[620px] md:w-[560px] lg:h-[790px] lg:w-[790px] xl:h-[760px] xl:w-[760px]"
+          list={assetsLists[0]}
         />
+        <Circle
+          className="internal-circle absolute flex h-[170px] w-[170px] items-center justify-center sm:top-[130px] sm:h-[204px] sm:w-[204px] md:h-[382px] md:w-[382px] lg:h-[520px] lg:w-[520px]"
+          list={assetsLists[1]}
+        />
+        <Circle
+          className="void-circle absolute hidden items-center justify-center xl:flex xl:h-[978px] xl:w-[978px]"
+          list={voidAssets}
+        />
+        <div className="void-assets-mask absolute z-10 hidden h-[calc(100%+200px)] w-screen xl:block" />
       </div>
-      <div className="absolute left-1/2 top-1/2 flex h-12.5 w-11 -translate-x-1/2 -translate-y-1/2 md:h-16 md:w-16 lg:h-[93px] lg:w-[93px] xl:z-10">
+      <div className="heart-gradient absolute-center absolute flex h-12.5 w-11 before:h-[360px] before:w-[360px] before:sm:h-[440px] before:sm:w-[440px] md:top-[calc(100%-56px)] md:h-16 md:w-16 before:md:h-[620px] before:md:w-[620px] lg:h-[93px] lg:w-[93px] before:lg:h-[800px] before:lg:w-[800px] xl:z-10 2xl:before:h-[950px] 2xl:before:w-[1000px]">
         <Image
           src={"/assets/icons/osmo-logo-icon.svg"}
-          alt="OSMO icon"
           width={45}
+          alt="OSMO icon"
           height={50}
-          className="h-auto w-auto -translate-y-[calc(100%+72px)] pb-[1.5px] md:-translate-y-6 md:pl-[4.45px] md:pr-[2.44px] lg:translate-y-8 lg:pb-[2.2px] lg:pl-[6.5px] lg:pr-[3.55px] 2xl:translate-y-14"
+          className="relative h-auto w-auto pb-[1.5px] md:pl-[4.45px] md:pr-[2.44px] lg:pb-[2.2px] lg:pl-[6.5px] lg:pr-[3.55px]"
         />
       </div>
-    </>
-  );
-}
-
-function Circle({
-  elements,
-  radius,
-  className,
-  isSemi,
-}: {
-  elements: TAsset[];
-  radius: number;
-  isSemi?: boolean;
-  className?: string;
-}) {
-  const numberOfElements = elements.length;
-  const diameter = radius * 2;
-  const angle = (isSemi ? 180 : 360) / numberOfElements;
-
-  return (
-    <div
-      className={cn("relative rounded-full", className)}
-      style={{ width: `${diameter}px`, height: `${diameter}px` }}
-    >
-      {elements.map((element, i) => (
-        <Element
-          key={`element ${i}`}
-          {...element}
-          style={
-            isSemi
-              ? {
-                  top: `${radius - radius * Math.sin((angle + angle / numberOfElements + 0.12) * i * (Math.PI / 180))}px`,
-                  right: `${radius - radius * Math.cos((angle + angle / numberOfElements + 0.12) * i * (Math.PI / 180))}px`,
-                }
-              : {
-                  top: `${radius - radius * Math.sin(angle * i * (Math.PI / 180))}px`,
-                  right: `${radius - radius * Math.cos(angle * i * (Math.PI / 180))}px`,
-                }
-          }
-        />
-      ))}
-    </div>
-  );
-}
-
-function Element({
-  className,
-  style,
-  iconUri,
-  name,
-  variation,
-}: {
-  className?: string;
-  style?: CSSProperties;
-} & TAsset) {
-  const isPositive = variation && variation.toDec().isPositive();
-  return (
-    <div
-      style={style}
-      className={cn(
-        "absolute z-10 flex h-10 w-10 -translate-y-1/2 translate-x-1/2 flex-col items-center justify-center md:h-20 md:w-20 lg:h-[110px] lg:w-24",
-        className,
-      )}
-    >
-      {iconUri && (
-        <Image
-          src={iconUri}
-          alt={`${name} logo`}
-          width={40}
-          height={40}
-          className="md:h-12 md:w-12 lg:h-18 lg:w-18"
-        />
-      )}
-      {variation && (
-        <div
-          className={cn(
-            "-mt-1.5 flex h-4 w-max items-center justify-center rounded-full p-1 md:h-5",
-            {
-              "bg-rust-500": !isPositive,
-              "bg-malachite-200": isPositive,
-            },
-          )}
-        >
-          {isPositive ? (
-            <Image
-              src={"/assets/explore-assets/indicator-up.svg"}
-              alt="Indicator Up"
-              width={12}
-              height={12}
-            />
-          ) : (
-            <Image
-              src={"/assets/explore-assets/indicator-down.svg"}
-              alt="Indicator Down"
-              width={12}
-              height={12}
-            />
-          )}
-          <span
-            className={cn(
-              "text-[10px] font-medium leading-[4.6px] -tracking-[0.132px] sm:text-xs",
-              {
-                "text-[#003F47]": isPositive,
-                "text-[#4A2706]": !isPositive,
-              },
-            )}
-          >
-            {formatPretty(variation)}
-          </span>
-        </div>
-      )}
     </div>
   );
 }
