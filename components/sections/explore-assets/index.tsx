ac093862ca2ca1fc662fe14180dd31cc3166a052
fabry-js <vivaldifabrizio10@gmail.com>
2024-03-29 15:41:33 +0100

    refactor(tw): extract leading-9.5

--- components/sections/explore-assets/index.tsx
+++ components/sections/explore-assets/index.tsx
@@ -122,7 +122,7 @@ export default function ExploreAssets() {
       <Heart assetsLists={[otherAssets, importantAssets]} />
       <div className="relative z-10 flex flex-col items-center justify-center gap-6 md:gap-8 2xl:px-6">
         <div className="flex flex-col items-center justify-center gap-4 text-center">
-          <h3 className="bg-explore-assets-headline xl:text-5.5xl 2xl:text-6.25xl 2xl:leading-17.5 w-[290px] font-poppins text-3.5xl font-medium leading-[38px] -tracking-[0.64px] sm:w-[390px] md:w-[420px] md:text-4xl md:leading-[41px] md:-tracking-[0.72px] lg:w-[594px] lg:text-5xl lg:leading-[55px] lg:-tracking-[0.96px] xl:w-[660px] xl:leading-[64px] xl:-tracking-[1.12px] 2xl:w-[768px] 2xl:-tracking-[1.28px]">
+          <h3 className="bg-explore-assets-headline xl:text-5.5xl 2xl:text-6.25xl 2xl:leading-17.5 leading-9.5 w-[290px] font-poppins text-3.5xl font-medium -tracking-[0.64px] sm:w-[390px] md:w-[420px] md:text-4xl md:leading-[41px] md:-tracking-[0.72px] lg:w-[594px] lg:text-5xl lg:leading-[55px] lg:-tracking-[0.96px] xl:w-[660px] xl:leading-[64px] xl:-tracking-[1.12px] 2xl:w-[768px] 2xl:-tracking-[1.28px]">
             The best opportunities are on Osmosis.
           </h3>
           <span className="md:leading-5.5 leading-6.25 max-w-[223px] font-light text-alpha-60 md:max-w-none lg:text-xl">
