33a7ccb436243f68638b802c071fb02431abbb1f
fabry-js <vivaldifabrizio10@gmail.com>
2024-04-01 16:30:15 +0200

    feat: use less bgs for osmo-cta section

--- components/sections/osmo-cta/index.tsx
+++ components/sections/osmo-cta/index.tsx
@@ -1,15 +1,15 @@
 import BackersSection from "@/components/sections/osmo-cta/backers-section";
-import Image from "next/image";
+import Image, { getImageProps } from "next/image";
 
 export default function OsmoCTASection() {
   return (
     <div className="relative">
       <section className="relative z-10 mt-12 flex flex-col items-center justify-center gap-2.5 self-stretch p-2 sm:mt-14 sm:p-4 md:gap-4 xl:mt-18 xl:gap-6 2xl:mt-20 2xl:gap-4 2xl:p-8 2xl:pb-0">
         {/**setting h-[470px] because there is no definitive copy yet for the subtitle on 2xl */}
-        <div className="bg-osmo-cta relative flex flex-col self-stretch rounded-3xl border border-solid border-white-full/10 px-6 pb-80 pt-8 sm:rounded-[32px] md:px-8 md:py-12 lg:p-12 2xl:h-[470px] 2xl:px-16 2xl:py-14">
-          <div className="flex flex-col justify-center gap-6 self-stretch md:gap-8 md:self-end xl:px-6 2xl:gap-10 2xl:px-0 2xl:py-8">
+        <div className="bg-gradient-osmo-cta relative flex flex-col self-stretch overflow-hidden rounded-3xl border border-solid border-white-full/10 px-6 pb-80 pt-8 sm:rounded-[32px] md:px-8 md:py-12 lg:p-12 2xl:h-[470px] 2xl:px-16 2xl:py-14">
+          <div className="relative z-10 flex flex-col justify-center gap-6 self-stretch md:gap-8 md:self-end xl:px-6 2xl:gap-10 2xl:px-0 2xl:py-8">
             <div className="flex flex-col justify-center gap-4 self-stretch md:self-start lg:gap-4">
-              <h5 className="-tracking-2% inline-flex flex-col gap-2 font-poppins text-3.5xl font-medium leading-8.75 text-neutral-100 sm:flex-row sm:items-center md:flex-col md:items-start md:text-4xl md:leading-10 lg:flex-row lg:items-center lg:gap-3 lg:text-5xl lg:leading-[53px] xl:text-5.5xl xl:leading-15.25 2xl:gap-1 2xl:text-6.25xl 2xl:leading-17.5">
+              <h5 className="inline-flex flex-col gap-2 font-poppins text-3.5xl font-medium leading-8.75 -tracking-2% text-neutral-100 sm:flex-row sm:items-center md:flex-col md:items-start md:text-4xl md:leading-10 lg:flex-row lg:items-center lg:gap-3 lg:text-5xl lg:leading-[53px] xl:text-5.5xl xl:leading-15.25 2xl:gap-1 2xl:text-6.25xl 2xl:leading-17.5">
                 <span>Powered by</span>
                 <span className="inline-flex items-center gap-2 lg:gap-3 2xl:p-2 2xl:pr-4">
                   OSMO
@@ -43,6 +43,7 @@ export default function OsmoCTASection() {
               </span>
             </button>
           </div>
+          <OsmoCTAIllustration />
         </div>
         <div className="flex flex-col gap-6 self-stretch py-4 lg:w-full lg:flex-row lg:items-center lg:justify-between lg:gap-0 lg:p-4 2xl:p-6">
           {/**row */}
@@ -175,5 +176,33 @@ export default function OsmoCTASection() {
         className="bottom absolute -bottom-[428px] z-0 hidden h-auto w-full 2xl:block"
       />
     </div>
+  );
+}
+
+function OsmoCTAIllustration() {
+  return (
+    <>
+      <Image
+        src={"/assets/osmo-cta/bg-lines.svg"}
+        alt=""
+        width={573}
+        height={503}
+        className="absolute -bottom-35 -left-28 h-[503px] w-[573px] max-w-none sm:-bottom-45 sm:h-[560px] sm:w-[637px] md:-bottom-[134px] md:-left-44 md:h-[520px] md:w-[593px] md:-scale-x-[1] lg:-bottom-[148px] lg:-left-42 lg:h-[568px] lg:w-[650px] xl:-left-19.5 2xl:-bottom-52 2xl:-left-[148px] 2xl:h-[757px] 2xl:w-[864px]"
+      />
+      <Image
+        src={"/assets/osmo-cta/coin.svg"}
+        alt=""
+        width={246}
+        height={246}
+        className="absolute bottom-4 left-8.5 sm:bottom-1 sm:left-16 sm:h-[268px] sm:w-[268px] md:hidden"
+      />
+      <Image
+        src={"/assets/osmo-cta/coin-md.svg"}
+        alt=""
+        width={251}
+        height={251}
+        className="absolute hidden md:bottom-15 md:left-3.5 md:block lg:bottom-0 lg:h-[318px] lg:w-[318px] xl:bottom-2 xl:left-28 2xl:bottom-4 2xl:left-24 2xl:h-[418px] 2xl:w-[418px]"
+      />
+    </>
   );
 }
