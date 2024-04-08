c80ec8e446e65fe718c4a57326312db71cfda96a
fabryscript <vivaldifabrizio10@gmail.com>
2024-04-09 01:49:57 +0200

    feat: add queryValidTokens

--- lib/queries/cms.ts
+++ lib/queries/cms.ts
@@ -3,6 +3,7 @@ import { queryAssetList } from "@/lib/queries/asset-list";
 import { queryAllTokens } from "@/lib/queries/numia";
 import { GITHUB_RAW_DEFAULT_BASEURL } from "@/lib/shared";
 import { LandingPageData } from "@/lib/types/cms";
+import { NumiaToken } from "@/lib/types/numia";
 import { unstable_cache } from "next/cache";
 
 const LANDING_PAGE_CMS_DATA_URL = new URL(
@@ -80,22 +81,54 @@ export const queryNewestAssetsSectionAssets = async (): Promise<
   }));
 };
 
+type NumiaTokenWithLogo = NumiaToken & { logoURIs: string };
+
+export const queryValidTokens = unstable_cache(
+  async (): Promise<NumiaTokenWithLogo[]> => {
+    const assets = await queryAllTokens();
+    const assetList = await queryAssetList();
+
+    const aggregatedAndFiltered: (NumiaTokenWithLogo | undefined)[] = assets
+      .map((asset) => {
+        const assetInfoAsset = assetList.assets.filter(
+          ({ coinMinimalDenom, verified, disabled, unstable }) => {
+            return (
+              coinMinimalDenom === asset.denom &&
+              verified &&
+              !disabled &&
+              !unstable
+            );
+          },
+        )[0];
+
+        if (!assetInfoAsset) return;
+
+        return {
+          ...asset,
+          logoURIs:
+            assetInfoAsset.logoURIs.svg ?? assetInfoAsset.logoURIs.png ?? "",
+        };
+      })
+      .filter(Boolean);
+
+    return aggregatedAndFiltered as NumiaTokenWithLogo[];
+  },
+  ["query-dust-filtered-assets"],
+  { revalidate: 86_400 },
+);
+
 export const queryTopGainersSectionAssets = async (): Promise<
   SectionAsset[]
 > => {
-  const assets = await queryAllTokens();
+  const aggregatedAndFiltered = await queryValidTokens();
 
-  const topGainers = assets
+  const topGainers = aggregatedAndFiltered
     .sort((a, b) => (a.price_24h_change! > b.price_24h_change! ? -1 : 1))
     .slice(0, 4);
 
-  return topGainers.map(({ symbol, name }) => ({
+  return topGainers.map(({ symbol, name, logoURIs }) => ({
     denom: symbol,
-    /**
-     * since we have no icons/logos returned from Numia
-     * we will elaborate them later in a separate server component
-     */
-    iconUri: "",
+    iconUri: logoURIs,
     name,
   }));
 };
