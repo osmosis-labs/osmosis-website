adb23a54ff57abd5badc80b9d90d7ae532fd8461
fabryscript <vivaldifabrizio10@gmail.com>
2024-04-08 12:33:26 +0200

    chore: remove test Promise

--- components/sections/stake-section/staking-apr.tsx
+++ components/sections/stake-section/staking-apr.tsx
@@ -3,8 +3,6 @@ import { RatePretty } from "@keplr-wallet/unit";
 import React from "react";
 
 export default async function StakingApr() {
-  // await new Promise<void>((res) => setTimeout(() => res(), 1000 * 2));
-
   const stakingApr = new RatePretty((await queryOsmoAPR())[0].apr / 100)
     .maxDecimals(2)
     .toString();
