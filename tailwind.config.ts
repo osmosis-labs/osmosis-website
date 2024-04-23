c7a1d3e04ae68eea4c9ba0957b3c28bb87e14cee
Davide Segullo <davidesegullo@gmail.com>
2024-04-23 10:22:25 +0200

    feat: :lipstick: disabled dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: ["class"],
+  darkMode: [],
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
