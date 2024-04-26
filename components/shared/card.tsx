f59d1d9c273ec0b254ce4c9b5dc805f109754011
fabryscript <vivaldifabrizio10@gmail.com>
2024-04-27 01:43:24 +0200

    feat(card): WIP remove react states

--- components/shared/card.tsx
+++ components/shared/card.tsx
@@ -44,86 +44,82 @@ export default function Card({
 }: CardProps) {
   return (
     <GTagLink asChild eventName="cardClicked" label={title}>
-      <Link
-        href={link ?? "#"}
-        className={cn(
-          "relative flex flex-grow flex-col justify-between self-stretch overflow-hidden rounded-2xl border border-solid border-osmoverse-650 bg-osmoverse-775 p-4 xl:rounded-3xl 2xl:p-6",
-          {
-            "has-[label[:hover]]:pointer-events-none": textExpandable,
-          },
-          // {
-          //   "pointer-events-none": isHover,
-          // },
-          className,
-        )}
-      >
-        {illustration}
-        {gradientOverlay}
-        <div className="relative z-20 flex items-center justify-between self-stretch">
-          <Image
-            src={iconUri}
-            alt={`${title} icon`}
-            className={cn(iconClassName)}
-            width={24}
-            height={24}
-          />
-          <div className="flex items-center">
-            {badgeDescription && (
-              <div
-                className={cn(
-                  "flex items-center justify-center gap-1 rounded-lg bg-[#87087C] px-3 py-1",
-                  badgeClassName,
-                )}
-              >
-                {badgeDescription}
-              </div>
-            )}
-            {link && linkArrowIconUri && (
+      <div className="relative w-full">
+        {textExpandable && (
+          <>
+            <input type="checkbox" id="descToggle" className="peer hidden" />
+            <label
+              htmlFor="descToggle"
+              className="pointer-events-auto absolute bottom-4 right-4 z-50 flex h-7 w-7 cursor-pointer items-center justify-center rounded-full bg-[#41366A] transition-transform peer-[:checked]:rotate-45 sm:hidden"
+            >
               <Image
-                src={linkArrowIconUri}
-                alt={`${title} link`}
-                width={40}
-                height={32}
-                className={cn("-ml-0.5", linkArrowClassName)}
+                src={"/assets/icons/cross.svg"}
+                alt="Open/Close description"
+                width={12}
+                height={12}
               />
-            )}
-          </div>
-        </div>
-        <div
+            </label>
+          </>
+        )}
+        <Link
+          href={link ?? "#"}
           className={cn(
-            "relative z-20 flex flex-col gap-2",
-            infoWrapperClassName,
+            "relative flex flex-grow flex-col justify-between self-stretch overflow-hidden rounded-2xl border border-solid border-osmoverse-650 bg-osmoverse-775 p-4 xl:rounded-3xl 2xl:p-6 peer-[:checked]:[&>div>p]:line-clamp-none",
+            className,
           )}
         >
-          <h3 className="font-poppins text-xl font-medium leading-6.5 text-neutral-100 2xl:text-2xl 2xl:leading-7.75">
-            {title}
-          </h3>
-          {textExpandable && (
-            <>
-              <input type="checkbox" id="descToggle" className="peer hidden" />
-              <label
-                htmlFor="descToggle"
-                className="pointer-events-auto absolute bottom-0 right-0 z-50 flex h-7 w-7 cursor-pointer items-center justify-center rounded-full bg-[#41366A] transition-transform peer-[:checked]:rotate-45 sm:hidden"
-              >
+          {illustration}
+          {gradientOverlay}
+          <div className="relative z-20 flex items-center justify-between self-stretch">
+            <Image
+              src={iconUri}
+              alt={`${title} icon`}
+              className={cn(iconClassName)}
+              width={24}
+              height={24}
+            />
+            <div className="flex items-center">
+              {badgeDescription && (
+                <div
+                  className={cn(
+                    "flex items-center justify-center gap-1 rounded-lg bg-[#87087C] px-3 py-1",
+                    badgeClassName,
+                  )}
+                >
+                  {badgeDescription}
+                </div>
+              )}
+              {link && linkArrowIconUri && (
                 <Image
-                  src={"/assets/icons/cross.svg"}
-                  alt="Open/Close description"
-                  width={12}
-                  height={12}
+                  src={linkArrowIconUri}
+                  alt={`${title} link`}
+                  width={40}
+                  height={32}
+                  className={cn("-ml-0.5", linkArrowClassName)}
                 />
-              </label>
-            </>
-          )}
-          <p
+              )}
+            </div>
+          </div>
+          <div
             className={cn(
-              "line-clamp-2 max-w-[448px] self-stretch leading-6.25 text-alpha-60 peer-[:checked]:line-clamp-none sm:line-clamp-none",
-              descriptionClassName,
+              "relative z-20 flex flex-col gap-2",
+              infoWrapperClassName,
             )}
           >
-            {description}
-          </p>
-        </div>
-      </Link>
+            <h3 className="font-poppins text-xl font-medium leading-6.5 text-neutral-100 2xl:text-2xl 2xl:leading-7.75">
+              {title}
+            </h3>
+            <p
+              className={cn(
+                "line-clamp-2 max-w-[448px] self-stretch leading-6.25 text-alpha-60 sm:line-clamp-none",
+                descriptionClassName,
+              )}
+            >
+              {description}
+            </p>
+          </div>
+        </Link>
+      </div>
     </GTagLink>
   );
 }
