ddc6b9a796cfde5fb7d3258a84dd6537e4cae432
Davide Segullo <davidesegullo@gmail.com>
2025-01-29 16:00:02 +0100

    feat: :wrench: remove redirect configs

--- next.config.mjs
+++ next.config.mjs
@@ -17,18 +17,6 @@ const nextConfig = {
           "https://drive.google.com/u/1/uc?id=1rKUX9X7EyJylDlrYT6wBIgziT53CQr7G",
         permanent: false,
       },
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "osmurica.com|osmosis-foundation.com",
-          },
-        ],
-        permanent: false,
-        destination:
-          "https://docs.google.com/document/d/1kiFYW90ECPtTb31urVHTOcACGIBXXr03-bPN5t7YvrE",
-      },
     ];
   },
 };
