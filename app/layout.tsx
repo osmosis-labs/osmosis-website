4bf0b145d42ad52bd76517325afec0f9c4df2ab0
Alessandro Lo Manto <45075112+alessandrolomanto@users.noreply.github.com>
2025-09-28 10:36:49 +0200

    Added csp header to allow vercel script

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,7 +63,7 @@ export default function RootLayout({
       <head>
         <meta 
           httpEquiv="Content-Security-Policy" 
-          content="script-src-elem 'self' https://www.googletagmanager.com https://static.cloudflareinsights.com https://vercel.live 'unsafe-inline';" 
+          content="default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://vercel.live https://www.googletagmanager.com https://static.cloudflareinsights.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data: https:; connect-src 'self' https://vercel.live;" 
         />
       </head>
       <GoogleTagManager nonce={nonce} gtmId={process.env.GTAG_ID ?? ""} />
